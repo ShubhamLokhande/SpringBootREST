f9d65d60cc241fb2c60b5ba68d642c1a8ec57d02
ShubhamLokhande <shubhamlok98@gmail.com>
2020-08-28 16:01:28 +0530

    War file generation

--- src/main/java/com/mobileTestingREST/MobileTestingRestApplication.java
+++ src/main/java/com/mobileTestingREST/MobileTestingRestApplication.java
@@ -2,9 +2,16 @@ package com.mobileTestingREST;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MobileTestingRestApplication {
+public class MobileTestingRestApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MobileTestingRestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileTestingRestApplication.class, args);
